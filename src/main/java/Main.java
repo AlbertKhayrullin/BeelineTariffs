df914abf14de5ff6757a8366a9d9dd49f4f2f585
AlbertKhayrullin <albertKh1620@rambler.ru>
2021-03-03 15:08:49 +0300

    src added

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        beelineTariffsProducts tarrifed = new beelineTariffsProducts();
+        tarrifed.nameTariff = "Близкие люди 1";
+        tarrifed.pricePerMonth = 350;
+        System.out.println(tarrifed.nameTariff);
+        System.out.println(tarrifed.pricePerMonth);
+    }
+}
---
+++ src/main/java/beelineTariffsProducts.java
@@ -0,0 +1,24 @@
+public class beelineTariffsProducts {
+    boolean supportSIM;
+    String nameTariff;
+    String URL_black;            // изображение общей опции на черном фоне Связь Z
+    String URL;                 //значок хит продаж
+    String URL_bag;                 // картинка чемодан
+    int limitInternet;
+    String unitLimitInternet;
+    int limitVoiceConnection;
+    String unitVoiceConnection;
+    int limitSMS;
+    boolean unlimitedInternet;
+    boolean unlimitedDistribution;
+    boolean unlimitedMessenger;
+    System freeConnection;       //бесплатные опции подключения дополнительных номеров
+    int channelTV;
+    String unitChannelTV;
+    String movieSubscription;
+    String discount;
+    String unitLimitSMS;
+    int pricePerMonth;
+    String unitPricePerMonth;
+    String bottomText;     // Узнать больше
+}
