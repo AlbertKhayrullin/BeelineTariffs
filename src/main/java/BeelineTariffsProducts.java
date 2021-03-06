b39ac1a3039ac959368942dfb92db80e655e02cb
AlbertKhayrullin <albertKh1620@rambler.ru>
2021-03-06 10:22:14 +0300

    fixed #3

--- src/main/java/BeelineTariffsProducts.java
+++ src/main/java/BeelineTariffsProducts.java
@@ -1,24 +1,13 @@
 public class BeelineTariffsProducts {
     boolean supportSIM;
     String nameTariff;
-    String urlBlack;            // изображение общей опции на черном фоне Связь Z
-    String url;                 //значок хит продаж
-    String urlBag;                 // картинка чемодан
     int limitInternet;
     String unitLimitInternet;
     int limitVoiceConnection;
     String unitVoiceConnection;
     int limitSMS;
-    boolean unlimitedInternet;
-    boolean unlimitedDistribution;
-    boolean unlimitedMessenger;
-    String freeConnection;       //бесплатные опции подключения дополнительных номеров
-    int channelTV;
-    String unitChannelTV;
-    String movieSubscription;
     String discount;
-    String unitLimitSMS;
     int pricePerMonth;
     String unitPricePerMonth;
-    String bottomText;     // Узнать больше
+    String bottomTextURL;
 }
